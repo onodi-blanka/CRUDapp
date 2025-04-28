3870b14092fd018c12ee5c53078467bca38f2820
blanka <blanka@DESKTOP-1UP747C>
2025-04-28 11:54:33 -0900

    WorkingAuthTests

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+export default {
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  testEnvironment: "node",
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { register, login } from "../controllers/auth.controllers.js";
+import { prisma } from "../db/prisma.js";
 
 const router = Router();
 
 // Register
-router.post("/register", register);
+router.post("/register", register(prisma));
 
 // Login
-router.post("/login", login);
+router.post("/login", login(prisma));
 
 export default router;
